4ec3dfb4c84c9a37b74170aed9157fdbe2c8d4c8
nightuser <nightuser.android@gmail.com>
2025-12-04 19:13:09 +0000

    Use segment trees for day 3

--- day03/day03.py
+++ day03/day03.py
@@ -1,45 +1,82 @@
+import dataclasses
 import sys
-from collections.abc import Iterable
+from collections.abc import Sized
 
 TOTAL_BATTERIES = 12
 
 
-def argmax[T](xs: Iterable[T]) -> tuple[int, T]:
-    return max(enumerate(xs), key=lambda ix: (ix[1], -ix[0]))
+@dataclasses.dataclass
+class Node:
+    index: int
+    value: int
+
+
+def node_key(node: Node):
+    return (node.value, -node.index)
+
+
+def node_max(first: Node, second: Node):
+    return max(first, second, key=node_key)
+
+
+class SegmentTree(Sized):
+    def __init__(self, bank: str):
+        self._total_bats = len(bank)
+        self._tree = [Node(0, 0) for _ in range(self._total_bats)]
+        for bat_index, bat_value in enumerate(bank):
+            self._tree.append(Node(bat_index, int(bat_value)))
+        for node_index in range(self._total_bats - 1, 0, -1):
+            self._tree[node_index] = node_max(
+                self._tree[2 * node_index], self._tree[2 * node_index + 1]
+            )
+
+    def max(self, left: int, right: int) -> Node:
+        left += self._total_bats
+        right += self._total_bats
+        result = Node(0, 0)
+        while left < right:
+            if left & 1 == 1:
+                result = node_max(result, self._tree[left])
+                left += 1
+            if right & 1 == 1:
+                right -= 1
+                result = node_max(result, self._tree[right])
+            left >>= 1
+            right >>= 1
+        return result
+
+    def __len__(self) -> int:
+        return self._total_bats
 
 
 def main() -> None:
     fname = sys.argv[1]
-    banks = list[tuple[int, ...]]()
+    banks = list[SegmentTree]()
     with open(fname) as fh:
         for line in fh:
             line = line.rstrip()
-            bank = tuple(int(c) for c in line)
+            bank = SegmentTree(line)
             banks.append(bank)
 
     ans1 = 0
     for bank in banks:
-        bat1_index, bat1_value = argmax(bank[:-1])
-        bat2_value = max(bank[bat1_index + 1 :])
-        joltage = bat1_value * 10 + bat2_value
+        bat1 = bank.max(0, len(bank) - 1)
+        bat2 = bank.max(bat1.index + 1, len(bank))
+        joltage = bat1.value * 10 + bat2.value
         ans1 += joltage
+    print(f"ans1 = {ans1}")
 
     ans2 = 0
     for bank in banks:
-        batteries = list[int]()
+        joltage = 0
         start_index = 0
         end_index = len(bank) + 1 - TOTAL_BATTERIES
         for _ in range(TOTAL_BATTERIES):
-            bat_index, bat_value = argmax(bank[start_index:end_index])
-            batteries.append(bat_value)
-            start_index += bat_index + 1
+            bat = bank.max(start_index, end_index)
+            joltage = 10 * joltage + bat.value
+            start_index = bat.index + 1
             end_index += 1
-        joltage = 0
-        for bat_value in batteries:
-            joltage = 10 * joltage + bat_value
         ans2 += joltage
-
-    print(f"ans1 = {ans1}")
     print(f"ans2 = {ans2}")
 
 
