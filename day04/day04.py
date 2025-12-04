2e43dcb1b93552fefcf5d92f2311759edac52586
nightuser <nightuser.android@gmail.com>
2025-12-04 17:57:21 +0000

    Further simplify day 4

--- day04/day04.py
+++ day04/day04.py
@@ -1,29 +1,45 @@
+import dataclasses
 import sys
 
 Point = tuple[int, int]
 
+
+@dataclasses.dataclass
+class NeighborData:
+    count: int
+    mask: int
+
+
+"""
+For every index `i`, DIRS[i] == -DIRS[7-i].
+"""
 DIRS = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
 
 
 def main() -> None:
     fname = sys.argv[1]
 
-    rolls = dict[Point, int]()
+    rolls = dict[Point, NeighborData]()
 
     with open(fname) as fh:
         for y, line in enumerate(fh):
             line = line.rstrip()
             for x, c in enumerate(line):
                 if c == "@":
-                    rolls[(x, y)] = 0
+                    rolls[(x, y)] = NeighborData(0, 0)
 
-    accessible_points = set[Point]()
+    accessible_points = list[Point]()
     for point in rolls:
         x, y = point
-        neighbors = [(x + dx, y + dy) for dx, dy in DIRS if (x + dx, y + dy) in rolls]
-        rolls[point] = len(neighbors)
-        if len(neighbors) < 4:
-            accessible_points.add(point)
+        record = rolls[point]
+        mask = 1
+        for dx, dy in DIRS:
+            if (x + dx, y + dy) in rolls:
+                record.count += 1
+                record.mask |= mask
+            mask <<= 1
+        if record.count < 4:
+            accessible_points.append(point)
     ans1 = len(accessible_points)
     print(f"ans1 = {ans1}")
 
@@ -32,12 +48,19 @@ def main() -> None:
         point = accessible_points.pop()
         x, y = point
         ans2 += 1
-        del rolls[point]
-        neighbors = [(x + dx, y + dy) for dx, dy in DIRS if (x + dx, y + dy) in rolls]
-        for neighbor in neighbors:
-            rolls[neighbor] -= 1
-            if rolls[neighbor] < 4:
-                accessible_points.add(neighbor)
+        neighbors_mask = rolls.pop(point).mask
+
+        for dir_id, (dx, dy) in enumerate(DIRS):
+            if neighbors_mask == 0:
+                break
+            if neighbors_mask & 1 == 1:
+                neighbor = (x + dx, y + dy)
+                neighbor_record = rolls[neighbor]
+                neighbor_record.count -= 1
+                neighbor_record.mask &= ~(1 << (len(DIRS) - dir_id - 1))
+                if neighbor_record.count == 3:
+                    accessible_points.append(neighbor)
+            neighbors_mask >>= 1
     print(f"ans2 = {ans2}")
 
 
