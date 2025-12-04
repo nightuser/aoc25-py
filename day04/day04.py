53cb317c169822f4a3d8786322591c9ab82cc0ad
nightuser <nightuser.android@gmail.com>
2025-12-04 16:11:45 +0000

    Refactor day 4

--- day04/day04.py
+++ day04/day04.py
@@ -1,53 +1,47 @@
 import sys
 
+Point = tuple[int, int]
+
 DIRS = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
 
 
 def main() -> None:
     fname = sys.argv[1]
-    width = None
-    height = 0
-    inner_grid = list[list[bool]]()
+
+    rolls = set[Point]()
+
     with open(fname) as fh:
-        for line in fh:
+        for y, line in enumerate(fh):
             line = line.rstrip()
-            if width is None:
-                width = len(line)
-            height += 1
-
-            row = [False] + [c == "@" for c in line] + [False]
-            inner_grid.append(row)
-
-    assert width is not None
-    empty_row = [False] * (width + 2)
-    grid = [empty_row] + inner_grid + [empty_row]
+            for x, c in enumerate(line):
+                if c == "@":
+                    rolls.add((x, y))
 
     ans1 = 0
-    for y in range(1, height + 1):
-        for x in range(1, width + 1):
-            if not grid[y][x]:
-                continue
-            around = sum(grid[y + dy][x + dx] for dx, dy in DIRS)
-            if around < 4:
-                ans1 += 1
+    for x, y in rolls:
+        neighbors = [(x + dx, y + dy) for dx, dy in DIRS if (x + dx, y + dy) in rolls]
+        if len(neighbors) < 4:
+            ans1 += 1
     print(f"ans1 = {ans1}")
 
     ans2 = 0
-    while True:
-        to_be_removed = list[tuple[int, int]]()
-        for y in range(1, height + 1):
-            for x in range(1, width + 1):
-                if not grid[y][x]:
-                    continue
-                around = sum(grid[y + dy][x + dx] for dx, dy in DIRS)
-                if around < 4:
-                    to_be_removed.append((x, y))
-        if not to_be_removed:
-            break
-        ans2 += len(to_be_removed)
-        for x, y in to_be_removed:
-            grid[y][x] = False
-    print(f"ans1 = {ans2}")
+    affected = rolls.copy()
+    next_affected = set[Point]()
+    while affected:
+        while affected:
+            p = affected.pop()
+            if p not in rolls:
+                continue
+            x, y = p
+            neighbors = [
+                (x + dx, y + dy) for dx, dy in DIRS if (x + dx, y + dy) in rolls
+            ]
+            if len(neighbors) < 4:
+                ans2 += 1
+                next_affected.update(neighbors)
+                rolls.remove(p)
+        affected, next_affected = next_affected, affected
+    print(f"ans2 = {ans2}")
 
 
 if __name__ == "__main__":
